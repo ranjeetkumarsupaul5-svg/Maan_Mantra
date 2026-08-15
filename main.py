c9d336bf821befa153f3cb8406db894f376a8bc6
Ranjeet Kumar <ranjeetkumarsupaul5@gmail.com>
2026-08-15 20:31:51 +0530

    Fix CORS configuration

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    #allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 model = joblib.load('Mental_Health_Score.pkl')
 
