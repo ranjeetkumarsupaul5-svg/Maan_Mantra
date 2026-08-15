7b3d845caf50a9c3303048e65ad2b628d5fe78d4
Ranjeet Kumar <ranjeetkumarsupaul5@gmail.com>
2026-08-15 20:44:57 +0530

    Update backend API URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://maan-mantra-1.onrender.com";
+  const API_BASE = "https://maan-mantra.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
